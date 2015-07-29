123369f4fe72716c758dd7642f53fa503fb2916a
luohong <846705189@qq.com>
2015-07-30 00:10:18 +0800

    添加.gitignore，防止提交无用代码

--- src/main/java/com/skg/luohong/user/controller/UserController.java
+++ src/main/java/com/skg/luohong/user/controller/UserController.java
@@ -3,6 +3,9 @@ package com.skg.luohong.user.controller;
 import com.skg.luohong.user.dao.IUserDao;
 import com.skg.luohong.user.entity.User;
 
+/**
+ * 测试
+ * */
 public class UserController {
 
 	private IUserDao userDao;
